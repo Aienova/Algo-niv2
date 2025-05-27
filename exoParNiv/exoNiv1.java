55bae3b661c0d38d9f5361e3d3a8338d2b8e8355
Aienova <eudes.konda@aienova.com>
2025-05-27 11:59:24 +0200

    exo 1.0

---
+++ exoParNiv/exoNiv1.java
@@ -0,0 +1,38 @@
+package exoParNiv;
+
+
+/* Niveau 1 : A l'aide d'une boucle, PRINT & SCANNER 
+*Créer un système de commande de restauration
+ * 
+ * 
+ * **********************AFFICHAGE ATTENDU ****************:
+ * 
+ * 
+ * Bonjour, votre commande est avec viande (true) ou vegetarien (false)
+ * true
+ * 
+ * (CETTE QUESTION NE S'AFFICHE PAS SI VOUS AVEZ CHOISI FALSE A LA PREMIERE)
+ * Votre steak vous le voulez saignant (true) ou à poing (false) ? 
+ * false
+ * 
+ * 
+ * Frites (true) ou riz (false)
+ * true
+ * 
+ * 
+ * Soda (true) ou eau gazeuse (false)
+ * false
+ * 
+ * 
+ * Merci poru votre commande, ça sera près dans 20min,
+ * 
+ * 
+ * 
+ */
+
+
+public class exoNiv1 {
+
+    
+    
+}
