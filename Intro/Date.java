676a67cd85d7e324a2b808c9e4b32af227c0e4dd
Aienova <eudes.konda@aienova.com>
2025-06-05 12:53:03 +0200

    v2.0

--- Intro/Date.java
+++ Intro/Date.java
@@ -1,24 +1,42 @@
 package Intro;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Date {
     public static void main(String[] args) {
 
-        
+
+        // Format de base des dates YYYY-MM-DD
+        // Exemple : 2025-05-21
+
+        try{
         // Current date and time
         LocalDateTime now = LocalDateTime.now();
 
         // Define a formatter
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM-yyyy HH:mm:ss");
+        // Print the current date and time
+        
+                System.out.println(now);
+
+        // y: Année, M: Mois, d: Jour, H: Heure (24h), m: Minute, s: Seconde
 
         // Format the date-time
         String formattedDate = now.format(formatter);
-        System.out.println("Formatted Date-Time: " + formattedDate);
+        System.out.println("Date d'aujourd'hui format Francophone: " + formattedDate);
 
         // Parse a string back to LocalDateTime
-        String dateTimeString = "2025-05-21 14:30:00";
+        String dateTimeString = "21/05/2025 14:30:00";
         LocalDateTime parsedDate = LocalDateTime.parse(dateTimeString, formatter);
-        System.out.println("Parsed Date-Time: " + parsedDate);
+        System.out.println("Conversion de date String en LocalDateTime: " + parsedDate);
+    }
+        catch(DateTimeParseException e){
+            System.out.println("Erreur de formatage de la date : ");
+        }
+
+         System.out.println("Continuer le code ! ");
+
+
     }
 }
--- Intro/Exception.java
+++ Intro/Exception.java
@@ -1,14 +1,22 @@
 package Intro;
 
+import java.util.Scanner; /* Importation de la bibliothèque Java Scanner */
+
 public class Exception {
     public static void main(String[] args) {
         // Exemple de gestion d'exception
-        try {
-            int result = 10/0;
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez un nombre pour le diviseur : ");
+        int diviseur = scanner.nextInt();
+        System.out.println("Entrez un nombre pour le dividende : ");
+        int dividende = scanner.nextInt();
+
+
+            int result = dividende / diviseur;
             System.out.println("Result: " + result);
-        } catch (ArithmeticException e) {
-            System.out.println("Erreur : Division par zéro non autorisée.");
-        }
+        
     }
 
 
